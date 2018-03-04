4fb37274cc417316a6ca61f36bdec86789021bb2
Khuong Pham <phkhuong.buh@gmail.com>
2018-03-04 18:17:31 +0700

    Add projects

---
+++ 1688035_Laptop_Khach_tham_quan_Web/1688035_Laptop_Khach_tham_quan_Web/3-Doi_tuong_va_Xu_ly/3B-Xu_ly_3L.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.IO;
+using System.Xml;
+using System.Globalization;
+using System.Net;
+//************************* View/Presentation -Layers VL/PL **********************************
+public partial class XL_THE_HIEN
+{
+    public static string Dia_chi_Media = $"{XL_LUU_TRU.Dia_chi_Dich_vu}/Media";
+    public static CultureInfo Dinh_dang_VN = CultureInfo.GetCultureInfo("vi-VN");
+
+
+    public static string Tao_Chuoi_HTML_Danh_sach_Laptop_Xem(XmlElement Danh_sach_Laptop)
+    {
+        var Chuoi_HTML_Danh_sach = "<div class='row'>";
+        foreach(XmlElement Laptop in Danh_sach_Laptop.GetElementsByTagName("Laptop"))
+        {
+            var Ten = Laptop.GetAttribute("Ten");
+            var Ma_so = Laptop.GetAttribute("Ma_so");
+            var Don_gia_Ban = long.Parse(Laptop.GetAttribute("Don_gia_Ban"));
+            var Chuoi_Chuc_nang_Chon = "<form method='post'>" +
+                                        "<input name='Th_Ma_so_Chuc_nang' type='hidden' value='CHON' />" +
+                                         $"<input name='Th_Ma_so_Laptop' type='hidden' value='{Ma_so}' />" +
+                                         $"<button type='submit' class='btn btn-primary hidden-sm-down btn-block ADD_BUTTON'>THÊM VÀO GIỎ HÀNG <img class='CART_ICON' src='{Dia_chi_Media}/CART_ICON.svg' /></button>" +
+                                       "</form>";
+            var Chuoi_Hinh = $"<div class='KHUNG_HINH mx-auto'>" +
+                                $"<a href='MH_Chi_tiet_San_pham.cshtml?Ma_so={Ma_so}'><img src='{Dia_chi_Media}/{Ma_so}.png' class='img-thumbnail HINH'/></a>" +
+                             "</div>";
+            var Chuoi_Thong_tin = $"<div>" +
+                                      $"<strong>{Ten}</strong>" +
+                                      $"<br />Đơn giá: { Don_gia_Ban.ToString("c0", Dinh_dang_VN) }" +
+                                  $"</div>";
+            var Chuoi_HTML = $"<div class='KHUNG col-6 col-sm-4 col-md-3 col-lg-2'>" +
+                                 $"<div class='THONG_TIN'>" +
+                                     $"{Chuoi_Hinh}" +
+                                     $"{Chuoi_Thong_tin}" +
+                                     $"{Chuoi_Chuc_nang_Chon}"+
+                                 $"</div>" +
+                             "</div>";
+            Chuoi_HTML_Danh_sach += Chuoi_HTML;
+        }
+        
+        Chuoi_HTML_Danh_sach += "</div>";
+        return Chuoi_HTML_Danh_sach;
+    }
+
+    public static string Tao_Chuoi_HTML_Danh_sach_Laptop_Chon(XmlElement Danh_sach_Laptop)
+    {
+
+        var Chuoi_HTML_Danh_sach = "<div class='row'  >";
+        if (Danh_sach_Laptop.ChildNodes.Count > 0)
+        {
+            Chuoi_HTML_Danh_sach = "<div class='row' style='border:1px solid blue;margin:5px;padding:5px'>";
+            var Chuoi_Chuc_nang_Dat_hang = "<form method='post'>" +
+                                       "<input name='Th_Ma_so_Chuc_nang' type='hidden' value='DAT_HANG' />" +
+                                        "<button type='submit' class='btn btn-danger'>Đặt hàng</button>" +
+                                      "</form>";
+            Chuoi_HTML_Danh_sach += Chuoi_Chuc_nang_Dat_hang;
+        }
+
+        foreach (XmlElement Laptop in Danh_sach_Laptop.GetElementsByTagName("Laptop"))
+        {
+            var Ten = Laptop.GetAttribute("Ten");
+            var Ma_so = Laptop.GetAttribute("Ma_so");
+            var Don_gia_Ban = long.Parse(Laptop.GetAttribute("Don_gia_Ban"));
+            var Ban_hang =  (XmlElement)Laptop.GetElementsByTagName("Ban_hang")[0];
+            var So_luong = Ban_hang.GetAttribute("So_luong");
+            var Chuoi_Hinh = $"<img src='{Dia_chi_Media}/{ Ma_so}.png' " +
+                             "style='width:90px;height:90px;' />";
+            var Chuoi_Chuc_nang_Huy_Chon = "<form method='post'>" +
+                                       "<input name='Th_Ma_so_Chuc_nang' type='hidden' value='HUY_CHON' />" +
+                                        $"<input name='Th_Ma_so_Laptop' type='hidden' value='{Ma_so}' />" +
+                                        "<button type='submit' class='btn btn-danger'>-</button>" +
+                                      "</form>";
+            var Chuoi_Thong_tin = $"<div class='btn' style='text-align:left'> " +
+                          $"<strong>{Ten}</strong>" +
+                          $"<br />{ Don_gia_Ban.ToString("c0", Dinh_dang_VN) }" +
+                          $"<br />Số lượng: { So_luong}"+
+                           $"{Chuoi_Chuc_nang_Huy_Chon}" +
+                          $"</div>";
+            var Chuoi_HTML = $"<div class='col-md-4' style='margin-bottom:10px' >" +
+                               $"{Chuoi_Hinh}" + $"{Chuoi_Thong_tin}" +
+                             "</div>";
+            Chuoi_HTML_Danh_sach += Chuoi_HTML;
+        }
+        Chuoi_HTML_Danh_sach += "</div>";
+
+        return Chuoi_HTML_Danh_sach;
+    }
+}
+//************************* Business-Layers BL **********************************
+public partial class XL_NGHIEP_VU
+{
+    public static XmlElement Tim_Laptop(string Ma_so, XmlElement Danh_sach_Laptop)
+    {
+        var Kq = (XmlElement)null;
+        foreach (XmlElement Laptop in Danh_sach_Laptop.GetElementsByTagName("Laptop"))
+        {
+            if (Ma_so == Laptop.GetAttribute("Ma_so"))
+                Kq = Laptop;
+
+        }
+        return Kq;
+    }
+    public static  XmlElement  Tra_cuu_Laptop(
+          string Chuoi_Tra_cuu, XmlElement   Danh_sach_Laptop)
+    {
+        Chuoi_Tra_cuu = Chuoi_Tra_cuu.ToUpper();
+        var Chuoi_Danh_sach_Kq = "<Danh_sach_Laptop />";
+        var Tai_lieu = new XmlDocument();
+        Tai_lieu.LoadXml(Chuoi_Danh_sach_Kq);
+        var Danh_sach_Kq = Tai_lieu.DocumentElement;
+        foreach(XmlElement Laptop in Danh_sach_Laptop.GetElementsByTagName("Laptop"))
+        {
+            var Ten = Laptop.GetAttribute("Ten");
+           
+            if (Ten.ToUpper().Contains(Chuoi_Tra_cuu ) )
+            {
+                var Laptop_Kq = Tai_lieu.ImportNode(Laptop, true );
+                Danh_sach_Kq.AppendChild(Laptop_Kq);
+            }
+        }
+        
+                    
+        return Danh_sach_Kq;
+    }
+
+    public static string Get_Ten_Chi_tiet_Laptop(XmlElement Laptop, string Chi_tiet)
+    {
+        var Chi_tiet_Element = (XmlElement)Laptop.GetElementsByTagName(Chi_tiet)[0];
+        return Chi_tiet_Element.GetAttribute("Ten");
+    }
+    
+    public static long Tinh_Tong_So_Laptop_Chon(XmlElement Danh_sach_Laptop)
+    {
+        var Kq = 0L;
+        foreach(XmlElement Laptop in Danh_sach_Laptop.GetElementsByTagName("Laptop"))
+        {
+            var Ban_hang = (XmlElement)Laptop.GetElementsByTagName("Ban_hang")[0];
+            Kq += long.Parse(Ban_hang.GetAttribute("So_luong"));
+        }
+        return Kq;
+    }
+}
+
+//************************* Data-Layers DL **********************************
+public partial class XL_LUU_TRU
+{
+    
+    public static string Dia_chi_Dich_vu = "http://localhost:61828";
+    static string Dia_chi_Dich_vu_Du_lieu = $"{Dia_chi_Dich_vu}/1-Dich_vu_Giao_tiep/DV_Khach_tham_quan.cshtml";
+
+    public static  XmlElement  Doc_Du_lieu()
+    {
+        var Chuoi_XML = "<Du_lieu />";
+        var Xu_ly = new WebClient();
+        Xu_ly.Encoding = System.Text.Encoding.UTF8;
+        var Tham_so = "Ma_so_Xu_ly=KHOI_DONG_DU_LIEU_KHACH_THAM_QUAN";
+        var Dia_chi_Xu_ly = $"{Dia_chi_Dich_vu_Du_lieu}?{Tham_so}";
+        try
+        {
+            var Chuoi_Kq = Xu_ly.DownloadString(Dia_chi_Xu_ly);
+            if (Chuoi_Kq.Trim() != "")
+                Chuoi_XML = Chuoi_Kq;
+
+
+        }
+        catch (Exception Loi)
+        {
+            Chuoi_XML = $"<Du_lieu Loi='{Loi.Message}'  />";
+        }
+
+        var Tai_lieu = new XmlDocument();
+        Tai_lieu.LoadXml(Chuoi_XML);
+        var Du_lieu = Tai_lieu.DocumentElement;
+        
+        return Du_lieu;
+    }
+    
+     
+
+}
---
+++ Cua_hang_Laptop_Dich_vu/Cua_hang_Laptop_Dich_vu/3-Doi_tuong_va_Xu_ly/3B-Xu_ly_3L.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.IO;
+using System.Xml;
+using System.Globalization;
+
+ 
+//************************* Business-Layers BL **********************************
+public partial class XL_NGHIEP_VU
+{   // PET : Minh họa Kỹ thuật đơn giản nhất - Không xét tính hiệu quả 
+    // ==> Có thể cải tiến 
+    public static XmlElement Tim_Laptop(
+          string Ma_so, XmlElement Du_lieu)
+    {
+
+        var Danh_sach_Laptop =(XmlElement) Du_lieu.GetElementsByTagName("Danh_sach_Laptop")[0];
+        var Kq= (XmlElement)null;
+        foreach (XmlElement Laptop in Danh_sach_Laptop.GetElementsByTagName("Laptop"))
+        {
+            if (Ma_so == Laptop.GetAttribute("Ma_so") )
+                Kq = Laptop;
+             
+        }
+
+
+        return Kq;
+    }
+    public static XmlElement Tao_Doi_tuong_Con(string Chuoi_XML, XmlElement Cha)
+    {
+        var Doi_tuong_Kq = (XmlElement)null;
+        try
+        {
+        
+            var Tai_lieu = new XmlDocument();
+            Tai_lieu.LoadXml(Chuoi_XML);
+            var Doi_tuong = Tai_lieu.DocumentElement;
+            Doi_tuong_Kq = (XmlElement) Cha.OwnerDocument.ImportNode(Doi_tuong, true);
+        }
+        catch(Exception Loi)
+        {
+
+        }
+        return Doi_tuong_Kq;
+    }
+
+    // Tạo Dữ liệu 
+    public static XmlElement Tao_Du_lieu_cua_Khach_Tham_quan(XmlElement Du_lieu_Ung_dung)
+    {
+
+        var Chuoi_XML = Du_lieu_Ung_dung.OuterXml;
+        var Tai_lieu = new XmlDocument();
+        Tai_lieu.LoadXml(Chuoi_XML);
+        var Du_lieu_He_khach = Tai_lieu.DocumentElement;
+        var Cong_ty_He_khach = (XmlElement)Du_lieu_He_khach.GetElementsByTagName("Cong_ty")[0];
+        var Danh_sach_Nhan_vien = Cong_ty_He_khach.GetElementsByTagName("Danh_sach_Nhan_vien")[0];
+        Cong_ty_He_khach.RemoveChild(Danh_sach_Nhan_vien);
+
+
+        foreach (XmlElement Laptop_He_khach in Du_lieu_He_khach.SelectNodes("//Laptop"))
+        {
+            var So_luong_Ton = Tinh_Ton_Kho_Laptop(Laptop_He_khach);
+            Laptop_He_khach.SetAttribute("So_luong_Ton", So_luong_Ton.ToString());
+            var Danh_sach_Ban_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Ban_hang")[0];
+            var Danh_sach_Dat_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Dat_hang")[0];
+            var Danh_sach_Nhap_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Nhap_hang")[0];
+            Laptop_He_khach.RemoveChild(Danh_sach_Ban_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Dat_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Nhap_hang);
+        }
+        return Du_lieu_He_khach;
+    }
+    public static XmlElement Tao_Du_lieu_cua_Nhan_vien_Ban_hang(XmlElement Du_lieu_Ung_dung)
+    {
+        var Chuoi_XML = Du_lieu_Ung_dung.OuterXml;
+        var Tai_lieu = new XmlDocument();
+        Tai_lieu.LoadXml(Chuoi_XML);
+        var Du_lieu_He_khach = Tai_lieu.DocumentElement;
+        var Cong_ty_He_khach = (XmlElement)Du_lieu_He_khach.GetElementsByTagName("Cong_ty")[0];
+        foreach (XmlElement Laptop_He_khach in Du_lieu_He_khach.SelectNodes("//Laptop"))
+        {
+            var So_luong_Ton = Tinh_Ton_Kho_Laptop(Laptop_He_khach);
+            Laptop_He_khach.SetAttribute("So_luong_Ton", So_luong_Ton.ToString());
+            var Danh_sach_Ban_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Ban_hang")[0];
+            var Danh_sach_Dat_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Dat_hang")[0];
+            var Danh_sach_Nhap_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Nhap_hang")[0];
+            Laptop_He_khach.RemoveChild(Danh_sach_Ban_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Dat_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Nhap_hang);
+        }
+
+        return Du_lieu_He_khach;
+    }
+    public static XmlElement Tao_Du_lieu_cua_Nhan_vien_Nhap_hang(XmlElement Du_lieu_Ung_dung)
+    {
+        var Chuoi_XML = Du_lieu_Ung_dung.OuterXml;
+        var Tai_lieu = new XmlDocument();
+        Tai_lieu.LoadXml(Chuoi_XML);
+        var Du_lieu_He_khach = Tai_lieu.DocumentElement;
+        var Cong_ty_He_khach = (XmlElement)Du_lieu_He_khach.GetElementsByTagName("Cong_ty")[0];
+        foreach (XmlElement Laptop_He_khach in Du_lieu_He_khach.SelectNodes("//Laptop"))
+        {
+            var So_luong_Ton = Tinh_Ton_Kho_Laptop(Laptop_He_khach);
+            Laptop_He_khach.SetAttribute("So_luong_Ton", So_luong_Ton.ToString());
+            var Danh_sach_Ban_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Ban_hang")[0];
+            var Danh_sach_Dat_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Dat_hang")[0];
+            var Danh_sach_Nhap_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Nhap_hang")[0];
+            Laptop_He_khach.RemoveChild(Danh_sach_Ban_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Dat_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Nhap_hang);
+        }
+
+        return Du_lieu_He_khach;
+    }
+    public static XmlElement Tao_Du_lieu_cua_Nhan_vien_Giao_hang(XmlElement Du_lieu_Ung_dung)
+    {
+        var Chuoi_XML = Du_lieu_Ung_dung.OuterXml;
+        var Tai_lieu = new XmlDocument();
+        Tai_lieu.LoadXml(Chuoi_XML);
+        var Du_lieu_He_khach = Tai_lieu.DocumentElement;
+        var Cong_ty_He_khach = (XmlElement)Du_lieu_He_khach.GetElementsByTagName("Cong_ty")[0];
+        foreach (XmlElement Laptop_He_khach in Du_lieu_He_khach.SelectNodes("//Laptop"))
+        {
+            var Danh_sach_Ban_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Ban_hang")[0];
+            var Danh_sach_Nhap_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Nhap_hang")[0];
+            Laptop_He_khach.RemoveChild(Danh_sach_Ban_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Nhap_hang);
+        }
+
+        return Du_lieu_He_khach;
+    }
+    public static XmlElement Tao_Du_lieu_cua_Quan_ly_Giao_hang(XmlElement Du_lieu_Ung_dung)
+    {
+        var Chuoi_XML = Du_lieu_Ung_dung.OuterXml;
+        var Tai_lieu = new XmlDocument();
+        Tai_lieu.LoadXml(Chuoi_XML);
+        var Du_lieu_He_khach = Tai_lieu.DocumentElement;
+        var Cong_ty_He_khach = (XmlElement)Du_lieu_He_khach.GetElementsByTagName("Cong_ty")[0];
+        foreach (XmlElement Laptop_He_khach in Du_lieu_He_khach.SelectNodes("//Laptop"))
+        {
+            var Danh_sach_Ban_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Ban_hang")[0];
+            var Danh_sach_Nhap_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Nhap_hang")[0];
+            Laptop_He_khach.RemoveChild(Danh_sach_Ban_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Nhap_hang);
+
+        }
+
+        return Du_lieu_He_khach;
+    }
+
+    public static XmlElement Tao_Du_lieu_cua_Quan_ly_Ban_hang(XmlElement Du_lieu_Ung_dung)
+    {
+        var Chuoi_XML = Du_lieu_Ung_dung.OuterXml;
+        var Tai_lieu = new XmlDocument();
+        Tai_lieu.LoadXml(Chuoi_XML);
+        var Du_lieu_He_khach = Tai_lieu.DocumentElement;
+        var Cong_ty_He_khach = (XmlElement)Du_lieu_He_khach.GetElementsByTagName("Cong_ty")[0];
+        foreach (XmlElement Laptop_He_khach in Du_lieu_He_khach.SelectNodes("//Laptop"))
+        {
+            var Doanh_thu = Tinh_Doanh_thu_Laptop(Laptop_He_khach);
+            var So_luong_Ton = Tinh_Ton_Kho_Laptop(Laptop_He_khach);
+            Laptop_He_khach.SetAttribute("Doanh_thu", Doanh_thu.ToString());
+            Laptop_He_khach.SetAttribute("So_luong_Ton", So_luong_Ton.ToString());
+            var Danh_sach_Dat_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Dat_hang")[0];
+            var Danh_sach_Nhap_hang = Laptop_He_khach.GetElementsByTagName("Danh_sach_Nhap_hang")[0];
+            Laptop_He_khach.RemoveChild(Danh_sach_Nhap_hang);
+            Laptop_He_khach.RemoveChild(Danh_sach_Dat_hang);
+        }
+
+        return Du_lieu_He_khach;
+    }
+
+    //==== Tính toán 
+
+    public static long Tinh_Doanh_thu_Laptop(XmlElement Laptop)
+    {
+        var Doanh_thu = 0L;
+        foreach (XmlElement Ban_hang in Laptop.GetElementsByTagName("Ban_hang"))
+        {
+            Doanh_thu += long.Parse(Ban_hang.GetAttribute("Tien"));
+        }
+        return Doanh_thu;
+    }
+
+    public static long Tinh_Ton_Kho_Laptop(XmlElement Laptop)
+    {
+        var Ton_Kho = 0L;
+        var So_Ban = 0L;
+        var So_Nhap = 0L;
+        foreach (XmlElement Ban_hang in Laptop.GetElementsByTagName("Ban_hang"))
+        {
+            So_Ban += long.Parse(Ban_hang.GetAttribute("So_luong"));
+        }
+        foreach(XmlElement Nhap_hang in Laptop.GetElementsByTagName("Nhap_hang"))
+        {
+            So_Nhap += long.Parse(Nhap_hang.GetAttribute("So_luong"));
+        }
+        Ton_Kho = So_Nhap - So_Ban;
+        return Ton_Kho;
+    }
+}
+
+
+
+//************************* Data-Layers DL **********************************
+public partial class XL_LUU_TRU
+{
+    static DirectoryInfo Thu_muc_Project = new DirectoryInfo(HostingEnvironment.ApplicationPhysicalPath);
+    static DirectoryInfo Thu_muc_Du_lieu = Thu_muc_Project.GetDirectories("2-Du_lieu_Luu_tru")[0];
+    static DirectoryInfo Thu_muc_Cong_ty = Thu_muc_Du_lieu.GetDirectories("Cong_ty")[0];
+    static DirectoryInfo Thu_muc_Laptop = Thu_muc_Du_lieu.GetDirectories("Laptop")[0];
+    static XmlElement Du_lieu;
+    public static XmlElement Doc_Du_lieu()
+    {   if (Du_lieu == null)
+        {
+            var Chuoi_XML = "<Du_lieu />";
+            var Tai_lieu = new XmlDocument();
+            Tai_lieu.LoadXml(Chuoi_XML);
+            Du_lieu = Tai_lieu.DocumentElement;
+            var Cong_ty = Doc_Danh_sach_Cong_ty().FirstChild;
+            Du_lieu.AppendChild(Tai_lieu.ImportNode(Cong_ty, true));
+            var Danh_sach_Laptop = Doc_Danh_sach_Laptop();
+            Du_lieu.AppendChild(Tai_lieu.ImportNode(Danh_sach_Laptop, true));
+        }
+        
+        return Du_lieu;
+    }
+    static XmlElement Doc_Danh_sach_Laptop()
+    {
+        var Chuoi_XML_Danh_sach = "<Danh_sach_Laptop />";
+        var Tai_lieu_Danh_sach = new XmlDocument();
+        Tai_lieu_Danh_sach.LoadXml(Chuoi_XML_Danh_sach);
+        var Danh_sach = Tai_lieu_Danh_sach.DocumentElement;
+        Thu_muc_Laptop.GetFiles("*.xml").ToList().ForEach(Tap_tin =>
+        {
+            var Duong_dan = Tap_tin.FullName;
+            var Tai_lieu = new XmlDocument();
+            Tai_lieu.Load(Duong_dan);
+            var Laptop = Tai_lieu.DocumentElement;
+            var Laptop_cua_Danh_sach = Tai_lieu_Danh_sach.ImportNode(Laptop, true);
+            Danh_sach.AppendChild(Laptop_cua_Danh_sach);
+        });
+        return Danh_sach;
+    }
+    static XmlElement Doc_Danh_sach_Cong_ty()
+    {
+        var Chuoi_XML_Danh_sach = "<Danh_sach_Cong_ty />";
+        var Tai_lieu_Danh_sach = new XmlDocument();
+        Tai_lieu_Danh_sach.LoadXml(Chuoi_XML_Danh_sach);
+        var Danh_sach = Tai_lieu_Danh_sach.DocumentElement;
+        Thu_muc_Cong_ty.GetFiles("*.xml").ToList().ForEach(Tap_tin =>
+        {
+            var Duong_dan = Tap_tin.FullName;
+            var Tai_lieu = new XmlDocument();
+            Tai_lieu.Load(Duong_dan);
+            var Cong_ty = Tai_lieu.DocumentElement;
+            var Cong_ty_cua_Danh_sach = Tai_lieu_Danh_sach.ImportNode(Cong_ty, true);
+            Danh_sach.AppendChild(Cong_ty_cua_Danh_sach);
+        });
+        return Danh_sach;
+    }
+
+
+    public static string Ghi_Dat_hang_Moi(XmlElement Laptop, XmlElement Dat_hang)
+    {
+        var Kq = "";
+        
+        try
+        {
+           
+            var Danh_sach_Dat_hang = Laptop.GetElementsByTagName("Danh_sach_Dat_hang")[0];
+            Danh_sach_Dat_hang.AppendChild(Dat_hang);
+            var Duong_dan = Thu_muc_Laptop.FullName + $"\\{Laptop.GetAttribute("Ma_so")}.xml";
+            var Chuoi_XML = Laptop.OuterXml;
+            File.WriteAllText(Duong_dan, Chuoi_XML);
+            Kq = "OK";
+        }
+        catch (Exception Loi)
+        {
+            Kq = Loi.Message;
+        }
+        if (Kq != "OK" && Laptop != null && Dat_hang != null)
+        {
+            var Danh_sach_Dat_hang = Laptop.GetElementsByTagName("Danh_sach_Dat_hang")[0];
+            Danh_sach_Dat_hang.RemoveChild(Dat_hang);
+        }
+        return Kq;
+
+    }
+
+    public static string Ghi_Ban_hang_Moi(XmlElement Laptop, XmlElement Ban_hang)
+    {
+        var Kq = "";
+
+        try
+        {
+
+            var Danh_sach_Ban_hang = Laptop.GetElementsByTagName("Danh_sach_Ban_hang")[0];
+            Danh_sach_Ban_hang.AppendChild(Ban_hang);
+            var Duong_dan = Thu_muc_Laptop.FullName + $"\\{Laptop.GetAttribute("Ma_so")}.xml";
+            var Chuoi_XML = Laptop.OuterXml;
+            File.WriteAllText(Duong_dan, Chuoi_XML);
+            Kq = "OK";
+        }
+        catch (Exception Loi)
+        {
+            Kq = Loi.Message;
+        }
+        if (Kq != "OK" && Laptop != null && Ban_hang != null)
+        {
+            var Danh_sach_Ban_hang = Laptop.GetElementsByTagName("Danh_sach_Ban_hang")[0];
+            Danh_sach_Ban_hang.RemoveChild(Ban_hang);
+        }
+        return Kq;
+
+    }
+}
